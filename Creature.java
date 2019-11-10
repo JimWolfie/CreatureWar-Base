e9d3e716f207b03c9f4d20da78428e039c0dec0a
JimWolfie <55092853+JimWolfie@users.noreply.github.com>
2019-11-10 14:09:54 -0500

    constructors

--- Creature.java
+++ Creature.java
@@ -1,18 +1,34 @@
 
 /**
- * Write a description of class Creature here.
+ * creature class for war project. parent class to different race sublcasses
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author nicholas lindgren
+ * @version 11/10/19
  */
+
 public class Creature
 {
    private int hp;
    private int strength;
-   
-   public Creature(){
+   /**
+    * constructor 
+    */
+   public Creature()
+   {
+       this.hp=10;
+       this.strength =10;
        
    }
+   /**
+    * Constructor 
+    * @param hp
+    * @param strength 
+    */
+   public Creature(int hp, int strength)
+   {
+       this.hp = hp; 
+       this.strength = strength;
+    }
     
    public int damage(){
        //TODO: change this
